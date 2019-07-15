7fc4ac446ffe12096d85fdded28181925783e4a5
DIDIDI <dguan@purdue.edu>
2019-07-15 17:17:15 +0800

    apartment entity

---
+++ src/main/java/aaa/project/entity/Apartment.java
@@ -0,0 +1,23 @@
+package aaa.project.entity;
+
+public class Apartment {
+    private String AptNum;
+    private Integer bedrooms;
+    private Integer bathrooms;
+    private Integer livingrooms;
+    private Double area;
+    private String facing;
+    private String city;
+    private String district;
+    private String community;
+    private String address;
+    private Boolean transportation;
+    private Integer rentType;
+    private String publishDate;
+    private Integer duration;
+    private Integer floor;
+    private Boolean elavator;
+    private String movein;
+    private Boolean tour;
+    private Boolean key;
+}
