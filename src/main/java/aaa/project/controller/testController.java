1cbccbf0022ea9564ec90d506ba1b5badf9e1ee9
DIDIDI <dguan@purdue.edu>
2019-07-15 17:39:40 +0800

    7.15 创建静态页面 signin-pjb details-fhl overview-zy

---
+++ src/main/java/aaa/project/controller/testController.java
@@ -0,0 +1,21 @@
+package aaa.project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class testController {
+    @RequestMapping("/signin")
+    public String toSignin(){
+        return "signin";
+    }
+
+    @RequestMapping("/details")
+    public String toDetails(){
+        return "details";
+    }
+    @RequestMapping("/overview")
+    public String toOverview(){
+        return "overview";
+    }
+}
