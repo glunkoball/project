24693c904229377520eac08d75117964df746be1
DIDIDI <dguan@purdue.edu>
2019-07-16 20:39:00 +0800

    主页面搭建

--- src/main/java/aaa/project/controller/JumpController.java
+++ src/main/java/aaa/project/controller/JumpController.java
@@ -4,8 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class testController {
-    /*@RequestMapping("/signin")
+public class JumpController {
+
+    @RequestMapping("/index")
+    public String toIndex(){
+        return "index";
+    }
+
+    @RequestMapping("/search")
+    public String toSearch(){
+        return "overview";
+    }
+
+
+    @RequestMapping("/signin")
     public String toSignin(){
         return "signin";
     }
@@ -14,13 +26,4 @@ public class testController {
     public String toDetails(){
         return "details";
     }
-    @RequestMapping("/overview")
-    public String toOverview(){
-        return "overview";
-    }
-
-    @RequestMapping("/index")
-    public String toIndex(){
-        return "index";
-    }*/
 }
